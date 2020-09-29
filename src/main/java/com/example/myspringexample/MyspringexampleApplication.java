fe29bf508ebd284bb74374c2c03b43fda8cf4d4f
manjo95 <selva.manoj@gmail.com>
2020-09-29 19:36:00 +0100

    Initial commit

---
+++ src/main/java/com/example/myspringexample/MyspringexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspringexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyspringexampleApplication.class, args);
+    }
+
+}
